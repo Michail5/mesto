f58a66e2ef0b93cfa224d5543c9cfb11bd294800
Michail5 <83210599+Michail5@users.noreply.github.com>
2021-07-21 09:32:31 +0300

    Add files via upload

---
+++ skripts/skript.js
@@ -0,0 +1,30 @@
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__input_type_name');
+let jobInput = document.querySelector('.popup__input_type_about');
+let editButton = document.querySelector('.profile__edit-button');
+let popup = document.querySelector('.popup');
+let closeButton = document.querySelector('.popup__close-button');
+let profileTitle = document.querySelector('.profile__title');
+let profileSubtitle = document.querySelector('.profile__subtitle');
+
+let popupOpen = () => {//добавить
+  popup.classList.add('popup_opened');
+}
+let popupClose = () => {//удалит
+  popup.classList.remove('popup_opened');
+}
+let formSubmitHandler = evt => {
+    evt.preventDefault();
+    profileTitle.textContent = nameInput.value;
+    profileSubtitle.textContent = jobInput.value;
+    popupClose();
+}
+let editProfile = () => {
+  popupOpen();
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileSubtitle.textContent;
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
+closeButton.addEventListener('click', popupClose);
+editButton.addEventListener('click', editProfile);
