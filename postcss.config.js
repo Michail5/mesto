ca5f714fb8cf9a3f37765d319fc0b20291471d8f
Michail5 <83210599+Michail5@users.noreply.github.com>
2021-09-24 22:19:29 +0300

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,10 @@
 // postcss.config.js
 
-// подключите плагины в файл
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 
 module.exports = {
-  // подключите плагины к PostCSS
+ 
   plugins: [
     // подключите autoprefixer
     autoprefixer,
